7eb713ebc634a2e2fdf6a034c00ca0eccdd690e6
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-16 10:05:00 +0100

    termin class voiture moto camion

--- main.php
+++ main.php
@@ -39,6 +39,72 @@ abstract class  Vehicule
    
    
  }
+ 
+ class Voiture extends Vehicule implements ReservableInterface{
+    private $nbPortes;
+    private $transmission;
+    
+    public function __construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible,$nbPortes,$transmission){
+      parent::__construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible);
+      $this->nbPortes=$nbPortes;
+      $this->transmission= $transmission;
+    }
+    
+    public function reserver(Client $client, DateTime $dateDebut, int $nbJours){
+        $reservation=new Reservation($this,Client $client, DateTime $dateDebut, int $nbJours);
+      return ;
+    }
+
+    public function getType(){
+        return $this->marque;
+    }
+
+    public function afficherDetails(){
+        return "Capacite Tonnage: $this->capaciteTonnage". parent::afficherDetails();
+    }
+  }
+
+  class Moto extends Vehicule implements ReservableInterface{
+    private $cylindree;
+    
+    public function __construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible,$cylindree){
+        parent::__construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible);
+      $this->cylindree=$cylindree;
+    }
+    
+    public function reserver(Client $client, DateTime $dateDebut, int $nbJours){
+      return new Reservation($this, Client $client, DateTime $dateDebut, int $nbJours);
+    }
+
+    public function getType(){
+        return $this->marque;
+    }
+
+    public function afficherDetails(){
+        return "Capacite Tonnage: $this->capaciteTonnage". parent::afficherDetails();
+    }
+  }
+  class Camion extends Vehicule implements ReservableInterface{
+    private $capaciteTonnage;
+    
+    public function __construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible,$capaciteTonnage){
+        parent::__construnct($id,$immatriculation,$marque,$modele,$prixJour,$disponible);
+      $this->capaciteTonnage=$capaciteTonnage;
+    }
+    
+    public function reserver(Client $client, DateTime $dateDebut, int $nbJours){
+      return new Reservation($this, Client $client, DateTime $dateDebut, int $nbJours);
+    }
+
+    public function getType(){
+        return $this->marque;
+    }
+
+    public function afficherDetails(){
+        return "Capacite Tonnage: $this->capaciteTonnage". parent::afficherDetails();
+    }
+  }
+  
 	
 	
 	
