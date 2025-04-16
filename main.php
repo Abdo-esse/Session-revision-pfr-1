2f5bd8cd0b1aee0517f28bb746436cfcd157340e
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-16 10:09:51 +0100

    termin la class person

--- main.php
+++ main.php
@@ -104,6 +104,23 @@ abstract class  Vehicule
         return "Capacite Tonnage: $this->capaciteTonnage". parent::afficherDetails();
     }
   }
+
+  abstract class  Personne{
+
+    protected $nom;
+   protected $prenom;
+   protected $email;
+   
+   public function __construnct($nom,$prenom,$email){
+     $this->nom=$nom;
+     $this->prenom=$prenom;
+     $this->email=$email;
+     
+   }
+
+   public abstract  function afficherProfil();
+  }
+
   
 	
 	
