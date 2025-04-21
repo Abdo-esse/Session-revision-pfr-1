5f7fb27d71e73149ae359cf277c5428ee9b0b46f
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-21 17:23:13 +0100

    termin du tp sql

--- sql/script.sql
+++ sql/script.sql
@@ -20,3 +20,35 @@ FROM utilisateurs u
 join profils p on p.utilisateur_id=u.id
 join adresses  a on a.utilisateur_id=u.id
 WHERE u.role='freelance' and p.disponible=true
+-- 4-Lister tous les utilisateurs qui ne possèdent pas encore de profil.
+select u.*
+FROM utilisateurs u
+RIGHT join profils p on p.utilisateur_id=u.id
+
+-- 5-Afficher les clients qui n'ont jamais publié de projet.
+select u.*
+From utilisateurs u 
+RIGHT join projets p on p.client_id=u.id
+-- 6-Afficher les projets ouverts avec leur budget et leur nombre total d’offres reçues.
+select p.*,count(o.projet_id) as  nombre_total_d’offres_reçues
+from projets p
+join offres o on o.projet_id=p.id
+WHERE p.statut='ouvert'
+-- 7-Lister les offres envoyées par des freelances dont le tarif horaire est inférieur à 100 MAD.
+select o.*,p.tarif_horaire
+from utilisateurs u 
+join profils p on p.utilisateur_id=u.id
+JOIN offres o on o.freelance_id=u.id
+WHERE p.tarif_horaire<100
+-- 8-Afficher les projets qui ont reçu au moins 3 offres
+select p.*,count(o.id)
+from projets p
+join offres o on o.projet_id=p.id
+group by p.id
+HAVING count(o.id)<3
+-- 10-Afficher les projets terminés avec les dates de début et de fin des missions associées.
+select o.*,m.date_debut,m.date_fin
+from projets p
+join offres o on o.projet_id=p.id
+join missions m on m.offre_id=o.id
+WHERE p.statut='terminé'
